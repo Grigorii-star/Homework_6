35a1dd8cbafdf0b97162e3fe5a1fd5be2aa1aca3
grigoriirarog <rarog.grigoriii@gmail.com>
2023-04-26 14:20:32 +0200

    Сделал ДЗ 6

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,94 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
+        task9();
+        task10();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        for (int i = 1; i < 11; i++) {
+            System.out.println("Итерация цикла " + i);
+        }
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+        for (int i = 10; i > 0; i--) {
+            System.out.println("Итерация цикла " + i);
+        }
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3");
+        for (int i = 2; i < 17; i = i + 2) {
+            System.out.println("Итерация цикла " + i);
+        }
+    }
+
+    public static void task4() {
+        System.out.println("Задача 4");
+        for (int i = 10; i > -11; i--) {
+            System.out.println("Итерация цикла " + i);
+        }
+    }
+
+    public static void task5() {
+        System.out.println("Задача 5");
+        for (int i = 1904; i < 2100; i = i + 4) {
+            System.out.println(i + " год является високосным");
+        }
+    }
+
+    public static void task6() {
+        System.out.println("Задача 6");
+        for (int i = 7; i < 105; i = i + 7) {
+            System.out.println("Итерация цикла " + i);
+        }
+    }
+
+    public static void task7() {
+        System.out.println("Задача 7");
+        for (int i = 1; i < 513; i = i * 2) {
+            System.out.println("Итерация цикла " + i);
+        }
+    }
+
+    public static void task8() {
+        System.out.println("Задача 8");
+        int money = 29_000;
+        int month = 0;
+        for (int i = 1; i < 13; i++) {
+            month += money;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + month);
+        }
+    }
+
+    public static void task9() {
+        System.out.println("Задача 9");
+        int money = 29_000;
+        double month = 0;
+        for (int i = 1; i < 13; i++) {
+            month += month / 100;
+            month += money;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + month);
+        }
+    }
+
+    public static void task10() {
+        System.out.println("Задача 10");
+        int a = 2;
+        int count;
+        for (int i = 1; i < 11; i++) {
+            count = i * a;
+            System.out.println(a + " * " + i + " = " + count);
+        }
     }
 }
